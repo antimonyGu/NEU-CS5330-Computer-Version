462230669f9275660e5cb4f9254bc2970de97a4c
zzgu <zzgu@pixelshift.ai>
2023-11-24 10:47:36 -0800

    project3

---
+++ project3/src/classifier.cpp
@@ -0,0 +1,216 @@
+#include "classifier.h"
+
+void clearFile(std::__cxx11::basic_string<char> file_name) {
+  std::ofstream f(file_name, std::ofstream::out | std::ofstream::trunc);
+  if (f.good()) {
+    f.open(file_name, std::ofstream::out | std::ofstream::trunc);
+  }
+  f.close();
+}
+
+int open_db(std::fstream &db_file, char mode, std::string file_name) {
+  if (db_file.is_open()) {
+    db_file.close();
+  }
+  db_file.open(file_name, mode == 'a' ? std::ios_base::app : std::ios_base::in);
+  return 0;
+}
+
+bool is_empty(std::fstream &db_file, std::string file_name)
+{
+  open_db(db_file, 'r', file_name);
+  return db_file.peek() == std::fstream::traits_type::eof();
+}
+
+int write_features(std::fstream &db_file, const std::string& feature_name, const std::vector<double>& features, std::string file_name) {
+  open_db(db_file, 'a', file_name);
+  db_file << feature_name << ":";
+  for (double value: features) {
+    db_file << value << ",";
+  }
+  db_file << std::endl;
+  return 0;
+}
+
+int read_features(std::fstream &db_file, std::map<std::string, std::vector<std::vector<double>>> &features, std::string file_name) {
+  open_db(db_file, 'r', file_name);
+  std::string line;
+  while (std::getline(db_file, line)) {
+    int colon_index = line.find(':');
+    // get the label name by splitting with delimiter as ":"
+    std::string label_name = line.substr(0, colon_index);
+    if (!features.count(label_name)) {
+      std::vector<std::vector<double>> label_features;
+      features.insert(std::make_pair(label_name, label_features));
+    }
+    std::vector<double> single_feature;
+    std::string features_str = line.substr(colon_index + 1, line.size());
+    int pos;
+    while ((pos = features_str.find(',')) != std::string::npos) {
+      // get each feature value by splitting with delimiter as ","
+      std::string value = features_str.substr(0, pos);
+      single_feature.emplace_back(std::atof(value.c_str()));
+      features_str.erase(0, pos + 1);
+    }
+    features[label_name].emplace_back(single_feature);
+  }
+  return 0;
+}
+
+// We will change the value of the vector, must not be the reference
+double euclidean_distance(std::vector<double> f1, std::vector<double> f2, std::vector<double> mean, std::vector<double> standard_deviation) {
+  double diff_square = 0.;
+
+  for (int i = 0; i < f1.size(); i++) {
+    f1[i] = (f1[i] - mean[i]) / standard_deviation[i];
+    f2[i] = (f2[i] - mean[i]) / standard_deviation[i];
+    diff_square += (f1[i] - f2[i]) * (f1[i] - f2[i]);
+  }
+  return std::sqrt(diff_square);
+}
+
+void mean_stddev_cal(const std::vector<std::vector<double>>& features, std::vector<double> &mean, std::vector<double> &standard_deviation) {
+  for (std::vector<double> single_feature: features) {
+    for (int i = 0; i < single_feature.size(); i++) {
+      mean[i] += single_feature[i];
+    }
+  }
+  for (int i = 0; i < mean.size(); i++) {
+    mean[i] /= features.size();
+  }
+
+  for (std::vector<double> single_feature: features) {
+    for (int i = 0; i < single_feature.size(); i++) {
+      standard_deviation[i] += (single_feature[i] - mean[i]) * (single_feature[i] - mean[i]);
+    }
+  }
+  for (int i = 0; i < standard_deviation.size(); i++) {
+    standard_deviation[i] /= features.size();
+    standard_deviation[i] = std::sqrt(standard_deviation[i]);
+  }
+}
+
+// read the features and calculate the mean and standard deviation for each feature
+void preprocess(std::fstream &db_file, int size, std::map<std::string, std::vector<std::vector<double>>> &features, std::vector<double> &mean, std::vector<double> &standard_deviation, std::string file_name) {
+  read_features(db_file, features, file_name);
+
+  // calculate the means for each feature vector
+  for (int i = 0; i < size; i++) {
+    mean.emplace_back(0.);
+    standard_deviation.emplace_back(0.);
+  }
+  std::vector<std::vector<double>> feature_vectors;
+  for (std::pair<std::string, std::vector<std::vector<double>>> p: features) {
+    for (const std::vector<double>& single_feature: p.second) {
+      feature_vectors.emplace_back(single_feature);
+    }
+  }
+  mean_stddev_cal(feature_vectors, mean, standard_deviation);
+}
+
+
+std::string nearest_neighbor_classifier(std::fstream &db_file, std::vector<double> &target_feature) {
+  std::map<std::string, std::vector<std::vector<double>>> features;
+  std::vector<double> mean;
+  std::vector<double> standard_deviation;
+  preprocess(db_file, (int)(target_feature.size()), features, mean, standard_deviation, FEATURE_FILE_NAME);
+
+  double min_dist = 100000.;
+  std::string min_label;
+  for (std::pair<std::string, std::vector<std::vector<double>>> p: features) {
+    for (const std::vector<double>& single_feature: p.second) {
+      double distance = euclidean_distance(single_feature, target_feature, mean, standard_deviation);
+      if(distance < min_dist) {
+        min_dist = distance;
+        min_label = p.first;
+      }
+    }
+  }
+  return min_label;
+}
+
+int knn_classifier(std::fstream &db_file, std::vector<double> &target_feature, int k, std::string &nearest_label) {
+  std::map<std::string, std::vector<std::vector<double>>> features;
+  std::vector<double> mean;
+  std::vector<double> standard_deviation;
+  preprocess(db_file, (int)(target_feature.size()), features, mean, standard_deviation, FEATURE_FILE_NAME);
+
+  // for each label, calculate the three nearest neighbor
+  std::map<std::string, double> label_distances;
+  for (std::pair<std::string, std::vector<std::vector<double>>> same_label_features: features) {
+    if (same_label_features.second.size() < k) {
+      return -1;
+    }
+    std::vector<double> distances;
+    for (std::vector<double> single_feature: same_label_features.second) {
+      double distance = euclidean_distance(single_feature, target_feature, mean, standard_deviation);
+      distances.emplace_back(distance);
+    }
+    double sum_k_distance = 0.;
+    for (int i = 0; i < k; i++) {
+      sum_k_distance += distances[i];
+    }
+    label_distances.insert(std::make_pair(same_label_features.first, sum_k_distance));
+  }
+  double min_dist = 10000.;
+//  std::string min_label;
+  for (std::pair<std::string, double> p: label_distances) {
+    if (p.second < min_dist) {
+      nearest_label = p.first;
+      min_dist = p.second;
+    }
+  }
+  return 0;
+}
+
+int evaluate(std::fstream &db_file, std::fstream &test_file, int k) {
+  std::map<std::string, std::vector<std::vector<double>>> train_features;
+  read_features(db_file, train_features, FEATURE_FILE_NAME);
+  std::map<std::string, std::vector<std::vector<double>>> test_features;
+  read_features(test_file, test_features, EVALUATE_FILE_NAME);
+
+  std::map<std::pair<std::string, std::string>, int> evaluation;
+  for (std::pair<std::string, std::vector<std::vector<double>>> p: test_features) {
+    for (std::pair<std::string, std::vector<std::vector<double>>> q: train_features) {
+      evaluation.insert(std::make_pair(std::make_pair(p.first, q.first), 0));
+    }
+  }
+
+  for (std::pair<std::string, std::vector<std::vector<double>>> single_label_features: test_features) {
+    for (std::vector<double> single_test_feature: single_label_features.second) {
+      std::string output_label;
+      if (k > 1) {
+        int status = knn_classifier(db_file, single_test_feature, k, output_label);
+        if (status == -1) {
+          std::cout << "At least " << k << " examples in each class!" << std::endl;
+          return -1;
+        }
+      } else {
+        output_label = nearest_neighbor_classifier(db_file, single_test_feature);
+      }
+      std::pair<std::string, std::string> key = std::make_pair(single_label_features.first, output_label);
+      evaluation[key] += 1;
+    }
+  }
+
+  // write the evaluation result to a file
+  clearFile(EVALUATE_OUTPUT_FILE_NAME);
+  open_db(db_file, 'a', EVALUATE_OUTPUT_FILE_NAME);
+  db_file << "\t\t";
+  std::vector<std::string> header;
+  for (std::pair<std::string, std::vector<std::vector<double>>> q: train_features) {
+    db_file << q.first << "\t";
+    header.emplace_back(q.first);
+  }
+  db_file << std::endl;
+
+  for (std::pair<std::string, std::vector<std::vector<double>>> p: test_features) {
+    db_file << p.first << "\t";
+    for (std::string colum: header) {
+      db_file << evaluation[std::make_pair(p.first, colum)] << "\t";
+    }
+    db_file << std::endl;
+  }
+  db_file.close();
+  return 0;
+}
---
+++ project3/src/client.cpp
@@ -0,0 +1,231 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "retrieval.h"
+#include "classifier.h"
+
+enum mode {
+  SEGMENTATION = 1,
+  TRAIN_DATA_PREP = 2,
+  TRAIN_SAVE = 3,
+  NN = 4,
+  KNN = 5,
+  TEST_DATA_PREP = 6,
+  EVALUATE_SAVE = 7,
+  EVALUATE = 8,
+} mode;
+
+int main(int argc, char *argv[]) {
+  // if the feature file exists and evaluation result file exist, clear them at first
+  clearFile(FEATURE_FILE_NAME);
+  clearFile(EVALUATE_FILE_NAME);
+
+  // number of component shown, ordered by area size
+  int component_num = 5;
+  // minimum size of area to be shown
+  int min_area = 1000;
+  mode = SEGMENTATION;
+  std::fstream db_file;
+  std::fstream test_file;
+  // set up k for knn
+  int k = 3;
+  // set up steps to shrink or grow
+  int steps = 5;
+
+  cv::VideoCapture *capdev;
+  // open the video device
+  capdev = new cv::VideoCapture(0);
+  if (!capdev->isOpened()) {
+    printf("Unable to open video device\n");
+    return (-1);
+  }
+  // get some properties of the image
+  cv::Size refS((int) capdev->get(cv::CAP_PROP_FRAME_WIDTH),
+                (int) capdev->get(cv::CAP_PROP_FRAME_HEIGHT));
+  printf("Expected size: %d %d\n", refS.width, refS.height);
+  cv::namedWindow("Video", 1); // identifies a window
+  cv::Mat frame;
+  std::vector<double> single_feature_vector;
+  for (;;) {
+    *capdev >> frame; // get a new frame from the camera, treat as a stream
+    if (frame.empty()) {
+      printf("frame is empty\n");
+      break;
+    }
+
+    // thresholding the image
+    cv::Mat threshold_image(frame.rows, frame.cols, CV_8UC1, cv::Scalar(0));
+    threshold(frame, threshold_image, THRESHOLD);
+
+    // clean the image with shrink and grow
+    cv::Mat cleaned_img(frame.rows, frame.cols, CV_8UC1, cv::Scalar(0));
+    cleanup(threshold_image, cleaned_img, steps);
+
+    cv::imshow("Video", frame);
+    cv::imshow("Threshold", threshold_image);
+    cv::imshow("cleanup", cleaned_img);
+
+    // segment the mat into different components, the major component is the one with the largest area(excluding background)
+    std::map<int, cv::Mat> regions;
+    cv::Mat components_img(frame.rows, frame.cols, CV_8UC3, cv::Scalar(0));
+    cv::Mat major_component(frame.rows, frame.cols, CV_8UC1, cv::Scalar(0));
+    int status = segment(cleaned_img, components_img, component_num, regions, min_area, major_component);
+
+    int window_size = (int)regions.size();
+    cv::Mat segmentation_img = cv::Mat(frame.rows, window_size * frame.cols, CV_8UC1);
+
+    // if no component detected, show the pure background image with a sign "No component detected"
+    if (status != 0) {
+      cv::Mat pure_black = cv::Mat(frame.rows, frame.cols, CV_8UC3, cv::Scalar(0));
+      cv::putText(pure_black,
+                  "No component detected",
+                  cv::Point(frame.rows / 2, frame.cols / 2),
+                  cv::FONT_HERSHEY_COMPLEX_SMALL,
+                  1.0,
+                  cv::Scalar(0, 0, 255));
+      cv::imshow("components", pure_black);
+      cv::imshow("segmentation", pure_black);
+    } else {
+      // if you are in training data or test data preparation, it will only mark the major component
+      if (mode == TRAIN_DATA_PREP || mode == TEST_DATA_PREP) {
+        std::vector<cv::Point> draw_vertices;
+        single_feature_vector.clear();
+        features(major_component, single_feature_vector, draw_vertices);
+        mark_object(components_img, draw_vertices);
+      } else if (mode == TRAIN_SAVE) {
+        std::cout << "Saving current feature for training..." << std::endl;
+        std::cout << "Please type in the name for this object:";
+        std::string feature_name;
+        // within one thread, you cannot display windows and wait for the console input at the same time
+        cv::destroyAllWindows();
+
+        std::cin >> feature_name;
+        write_features(db_file, feature_name, single_feature_vector, FEATURE_FILE_NAME);
+        std::cout << "Save the feature successfully!" << std::endl << std::endl;
+        mode = TRAIN_DATA_PREP;
+        std::cout << "Back to TRAIN_DATA_PREP mode, the major component is marked" << std::endl;
+        std::cout << "Press 's' to save the feature or other keys to other modes!" << std::endl;
+      } else if (mode == EVALUATE_SAVE) {
+        std::cout << "Saving current feature for evaluating..." << std::endl;
+        std::cout << "Please type in the name for this object:";
+        std::string feature_name;
+        // within one thread, you cannot display windows and wait for the console input at the same time
+        cv::destroyAllWindows();
+
+        std::cin >> feature_name;
+        write_features(test_file, feature_name, single_feature_vector, EVALUATE_FILE_NAME);
+        std::cout << "Save the feature successfully!" << std::endl << std::endl;
+        mode = TEST_DATA_PREP;
+        std::cout << "Back to TEST_DATA_PREP mode, the major component is marked" << std::endl;
+        std::cout << "Press 's' to save the feature or other keys to other modes!" << std::endl;
+      } else {
+        // if you are in the default segmentation mode, it will show all the components in the window
+        for (std::pair<int, cv::Mat> region: regions) {
+          std::vector<double> feature_vector;
+          std::vector<cv::Point> draw_vertices;
+          features(region.second, feature_vector, draw_vertices);
+          mark_object(components_img, draw_vertices);
+          // show the result from NN algorithm in the mat
+          if (mode == NN) {
+            std::string label_name = nearest_neighbor_classifier(db_file, feature_vector);
+            cv::putText(components_img,
+                        "NN: " + label_name,
+                        draw_vertices[0],
+                        cv::FONT_HERSHEY_COMPLEX_SMALL,
+                        0.8,
+                        cv::Scalar(200, 200, 250));
+          }
+            // show the result from KNN algorithm in the mat
+          else if (mode == KNN) {
+            std::string label_name;
+            int status = knn_classifier(db_file, feature_vector, k, label_name);
+            if (status == -1) {
+              std::cout << "At least " << k << " examples in each class!" << std::endl;
+              std::cout << "Back to SEGMENTATION mode!" << std::endl;
+              mode = SEGMENTATION;
+            }
+            cv::putText(components_img,
+                        "KNN: " + label_name,
+                        draw_vertices[0],
+                        cv::FONT_HERSHEY_COMPLEX_SMALL,
+                        0.8,
+                        cv::Scalar(200, 200, 250));
+          }
+            // evaluate the features in "../test_features.txt" and write the confusion matrix in "../evaluation.txt"
+          else if (mode == EVALUATE) {
+            evaluate(db_file, test_file, k);
+            mode = SEGMENTATION;
+          }
+        }
+      }
+      // show the top k components
+      int index = 0;
+      for (auto p: regions) {
+        p.second.copyTo(segmentation_img.rowRange(0, frame.rows).colRange(index * frame.cols, (index + 1) * frame.cols));
+        index++;
+      }
+//        cv::imshow("segmentation", segmentation_img);
+      cv::imshow("segmentation", segmentation_img);
+      cv::imshow("components", components_img);
+    }
+
+    // see if there is a waiting keystroke
+    int key = cv::waitKey(10);
+    // if user types 'q', quit the program
+    if (key == 'q') {
+      break;
+    }
+    if (key == 't') {
+      mode = TRAIN_DATA_PREP;
+      std::cout << "You are in TRAIN_DATA_PREP mode, the major component is marked" << std::endl;
+      std::cout << "Press 's' to save the feature or space key to the SEGMENTATION mode!" << std::endl << std::endl;
+    } else if (key == ' ') {
+      mode = SEGMENTATION;
+      std::cout << "You are in segmentation mode!" << std::endl;
+    } else if (key == 's') {
+      if (mode != TRAIN_DATA_PREP && mode != TEST_DATA_PREP) {
+        std::cout << "Please enter the TRAIN_DATA_PREP mode or TEST_DATA_PREP mode at first!" << std::endl;
+      } else if (mode == TRAIN_DATA_PREP) {
+        mode = TRAIN_SAVE;
+      } else {
+        mode = EVALUATE_SAVE;
+      }
+    } else if (key == 'n') {
+      if (is_empty(db_file, FEATURE_FILE_NAME)) {
+        std::cout << "No features in database, please add some features in TRAIN_DATA_PREP mode" << std::endl;
+        mode = SEGMENTATION;
+      } else {
+        mode = NN;
+        std::cout << "You are in NEAREST_NEIGHBOR_CLASSIFY mode!" << std::endl;
+      }
+    } else if (key == 'k') {
+      if (is_empty(db_file, FEATURE_FILE_NAME)) {
+        std::cout << "No features in database, please add some features in TRAIN_DATA_PREP mode" << std::endl;
+        mode = SEGMENTATION;
+      } else {
+        mode = KNN;
+        std::cout << "You are in KNN mode!" << std::endl;
+      }
+    } else if (key == 'p') {
+      mode = TEST_DATA_PREP;
+      std::cout << "You are in TEST_DATA_PREP mode!" << std::endl;
+      std::cout << "Press 's' to save the feature or space key to the SEGMENTATION mode!" << std::endl << std::endl;
+    } else if (key == 'e') {
+      if (is_empty(db_file, EVALUATE_FILE_NAME)) {
+        std::cout << "No features to be evaluated, please add some features in TEST_DATA_PREP mode" << std::endl;
+        mode = SEGMENTATION;
+      } else {
+        mode = EVALUATE;
+        std::cout << "You are in EVALUATE mode!" << std::endl;
+        std::cout << "Confusion Matrix generated in: " << EVALUATE_OUTPUT_FILE_NAME << std::endl;
+      }
+    } else if (key == 'a') {
+      cv::imwrite("../origin.jpg", frame);
+      cv::imwrite("../threshold.jpg", threshold_image);
+      cv::imwrite("../cleanup.jpg", cleaned_img);
+      cv::imwrite("../segmentation.jpg", segmentation_img);
+      cv::imwrite("../components.jpg", components_img);
+    }
+  }
+  delete capdev;
+  return (0);
+}
